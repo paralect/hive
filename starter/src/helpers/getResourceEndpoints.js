784b4c12a97dd44349b82fb163a4655c20cf4b5c
Igor Krasnik <ihar.krasnik@gmail.com>
2024-07-03 11:38:48 +0200

    Fix docker build

--- starter/src/helpers/getResourceEndpoints.js
+++ starter/src/helpers/getResourceEndpoints.js
@@ -20,7 +20,6 @@ module.exports = async (resourceName) => {
     )).map( f => ({ name: f }))];
   }
 
-  console.log('endpointFiles', resourceName, endpointFiles);
   return endpointFiles.map(({ name, isHiveEndpoint }) => ({
     file: isHiveEndpoint ?  `${process.env.HIVE_SRC}/resources/${resourceName}/endpoints/${name}`: `${__dirname}/../resources/${resourceName}/endpoints/${name}`,
     name: _.last(name.split("/")).replace(".js", ""),
