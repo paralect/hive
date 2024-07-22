f7d69e96a077f7f92a2ae380208f767a71fa59ce
Igor Krasnik <ihar.krasnik@gmail.com>
2024-07-22 14:41:02 +0200

    Fix typo in scheduler

--- starter/src/scheduler.js
+++ starter/src/scheduler.js
@@ -3,7 +3,7 @@ const schedule = require("node-schedule");
 
 const requireDir = require("require-dir");
 
-requireDir(`${process.HIVE_SRC || "."}/scheduler/handlers`, {
+requireDir(`${process.env.HIVE_SRC || "."}/scheduler/handlers`, {
   mapValue: (handler, handlerName) => {
     console.log(
       `[scheduler] Registering handler ${handlerName} with cron ${handler.cron}`
