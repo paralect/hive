c9457d751bacff3f0a3b71e6d6b8cf1d6992cf46
pimp <wotik.com@gmail.com>
2024-07-23 13:46:37 +0300

    set up dev server, module aliases with tsx

--- starter/src/app.js
+++ starter/src/app.js
@@ -1,4 +1,3 @@
-import moduleAlias from "module-alias";
 import fs from "fs";
 import Koa from "koa";
 import http from "http";
@@ -15,31 +14,26 @@ import mount from "koa-mount";
 import routes from "routes";
 import get from "resources/health/endpoints/get";
 import "scheduler";
-moduleAlias.addPath(__dirname);
-if (process.env.HIVE_SRC) {
-    moduleAlias.addPath(process.env.HIVE_SRC);
-}
-moduleAlias()
 
 const main = async () => {
-    await db.init();
-    process.on("unhandledRejection", (reason, p) => {
-        console.trace(reason.stack);
-        logger.error(reason.stack);
-    });
-    const app = new Koa();
-    app.use(cors({ credentials: true }));
-    app.use(helmet());
-    qs(app);
-    app.use(bodyParser({ enableTypes: ["json", "form", "text"] }));
-    app.use(mount("/health", get.handler));
-    app.use(requestLogger());
-    routes(app);
-    const server = http.createServer(app.callback());
-    server.listen(config.port, () => {
+  await db.init();
+  process.on("unhandledRejection", (reason, p) => {
+    console.trace(reason.stack);
+    logger.error(reason.stack);
+  });
+  const app = new Koa();
+  app.use(cors({ credentials: true }));
+  app.use(helmet());
+  qs(app);
+  app.use(bodyParser({ enableTypes: ["json", "form", "text"] }));
+  app.use(mount("/health", get.handler));
+  app.use(requestLogger());
+  routes(app);
+  const server = http.createServer(app.callback());
+  server.listen(config.port, () => {
         console.log(`Api server listening on ${config.port}, in ${process.env.NODE_ENV} mode and ${process.env.APP_ENV} environment`);
-    });
-    await socketIo(server);
+  });
+  await socketIo(server);
 };
 main();
 export default main;
