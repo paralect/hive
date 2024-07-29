565b1219e76490124d04b058afdd5faafb3812b1
Igor Krasnik <ihar.krasnik@gmail.com>
2024-07-29 12:54:03 +0200

    Fix casing

--- starter/src/autoMap/schemaMappings.js
+++ starter/src/autoMap/schemaMappings.js
@@ -6,8 +6,8 @@ if (fs.existsSync('./schemaMappings.json')) {
   schemaMappings = JSON.parse(fs.readFileSync('./schemaMappings.json', 'utf8') || '{}');
 }
 
-if (process.env.HIVE_SRC && fs.existsSync(`${process.env.HIVE_SRC}/automap/schemaMappings.json`)) {
-  schemaMappings = JSON.parse(fs.readFileSync(`${process.env.HIVE_SRC}/automap/schemaMappings.json`, 'utf8') || '{}');
+if (process.env.HIVE_SRC && fs.existsSync(`${process.env.HIVE_SRC}/autoMap/schemaMappings.json`)) {
+  schemaMappings = JSON.parse(fs.readFileSync(`${process.env.HIVE_SRC}/autoMap/schemaMappings.json`, 'utf8') || '{}');
 }
 
 export default schemaMappings;
