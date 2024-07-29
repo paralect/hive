6489637f66ca7b20a1104a0851600e1d20383940
Igor Krasnik <ihar.krasnik@gmail.com>
2024-07-29 16:44:39 +0200

    Fix handler trigger

--- starter/src/resources/_dev/endpoints/triggerSchedulerHandler.js
+++ starter/src/resources/_dev/endpoints/triggerSchedulerHandler.js
@@ -12,7 +12,7 @@ export const requestSchema = z.object({
 export const handler = async (ctx) => {
   const { name } = ctx.validatedData;
 
-  const schedulerHandler = await (import(`scheduler/handlers/${name}`)).default;
+  const schedulerHandler = await (import(`scheduler/handlers/${name}`));
 
   try {
     const data = await schedulerHandler.handler();
