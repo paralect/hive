2b6b16c80ad983239005558914b61109312137ec
Igor Krasnik <ihar.krasnik@gmail.com>
2024-11-25 22:13:20 +0100

    attachUser

--- starter/src/middlewares/attachUser.js
+++ starter/src/middlewares/attachUser.js
@@ -1,9 +1,41 @@
+import db from 'db';
+import config from 'app-config';
 
-const middleware = async (ctx, next) => {
-  ctx.state.isAllowAnonymous = true;
-  return next();
+const userService = db.services.users;
+const tokenService = db.services.tokens;
+
+const storeTokenToState = async (ctx) => {
+  let accessToken = ctx.cookies.get('access_token');
+
+  const { authorization } = ctx.headers;
+
+  if (!accessToken && authorization) {
+    accessToken = authorization.replace('Bearer', '').trim();
+  }
+
+  if (!accessToken && config._hive.authHeaderName && ctx.headers[config._hive.authHeaderName]) {
+    accessToken = ctx.headers[config._hive.authHeaderName];
+  }
+
+  ctx.state.accessToken = accessToken;
 };
 
-middleware.runOrder = -1;
+export default async (ctx, next) => {
+  storeTokenToState(ctx);
 
-export default middleware;
+  let token;
+
+  if (ctx.state.accessToken) {
+    token = await tokenService.findOne({ token: ctx.state.accessToken });
+  }
+
+  if (token) {
+    ctx.state.user = await userService.findOne({ _id: token.user._id });
+
+    if (token.metadata) {
+      ctx.state.user.authMetadata = token.metadata;
+    }
+  }
+
+  await next();
+};
--- starter/src/middlewares/isAuthorized.js
+++ starter/src/middlewares/isAuthorized.js
@@ -1,56 +1,18 @@
-import db from 'db';
-import config from 'app-config';
-
-const userService = db.services.users;
-const tokenService = db.services.tokens;
-
-const storeTokenToState = async (ctx) => {
-  let accessToken = ctx.cookies.get('access_token');
-
-  const { authorization } = ctx.headers;
-
-  if (!accessToken && authorization) {
-    accessToken = authorization.replace('Bearer', '').trim();
-  }
-
-  if (!accessToken && config._hive.authHeaderName && ctx.headers[config._hive.authHeaderName]) {
-    accessToken = ctx.headers[config._hive.authHeaderName];
-  }
-
-  ctx.state.accessToken = accessToken;
-};
+import attachUser from './attachUser';
 
 export default async (ctx, next) => {
-  storeTokenToState(ctx);
-
-  let token;
-
   if (ctx.state.isSkipAuth) {
     ctx.state.user = null;
     return next();
   }
 
-  if (ctx.state.accessToken) {
-    token = await tokenService.findOne({ token: ctx.state.accessToken });
-  }
-
-  if (token) {
-    ctx.state.user = await userService.findOne({ _id: token.user._id });
-
-    if (token.metadata) {
-      ctx.state.user.authMetadata = token.metadata;
+  await attachUser(ctx, async () => {
+    if (ctx.state.user) {
+      return next();
     }
-  }
 
-  if (ctx.state.user) {
-    return next();
-  }
-
-  if (ctx.state.isAllowAnonymous) {
-    return next();
-  }
-
-  ctx.status = 401;
-  ctx.body = {};
-  return null;
+    ctx.status = 401;
+    ctx.body = {};
+    return null;
+  });
 };
